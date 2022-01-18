5c8cde306aeb16c196f0f5d78bf545be2b7e1414
Umar Khan <ufkhan97@gmail.com>
2022-01-18 16:15:12 +0000

    added listener functions but erroring

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import * as React from 'react'
+import React, {Component} from 'react'
 import './App.css'
 //import './web3storage/put-files.js'
 
-export default function App() {
-function foo(){
-   alert('how DARE you!! >:( ');
-}
+export class App extends Component {
+    state = {
+        selectedFile: null
+    }; 
 
-function bar(){
-    alert('You changed the file!');
-}
+    onFileChange = event => {
+        this.setState({ selectedFile: event.target.files[0] });
+    };
 
-  return (
+    foo():void {
+        alert('how DARE you!! >:( ');
+    };
+
+
+return (
+    <div>
       <main>
-      <h1>Hello World!</h1>
-      <h2>React⚛️ + Vite⚡ + Replit🌀</h2>
-      <button type="button" onClick={foo}>Do NOT Click Me!</button>
-      <br />
-      <input type="file" onChange={bar}/>
-      <button type ="button">Upload File</button>
-    </main>
+        <h1>Hello World!</h1>
+        <h2>React⚛️ + Vite⚡ + Replit🌀</h2>
+       // <button type="button" onClick={this.foo}>Do NOT Click Me!</button>
+        <br />
+       // <input type="file" onChange={this.onFileChange}/>
+        //<button type ="button">Upload File</button>
+     </main>
+    </div>
   )
+
 }
